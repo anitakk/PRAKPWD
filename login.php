43e13aa2cafe7396bd36575621df6b9f283c65ab
anitakk <92177110+anitakk@users.noreply.github.com>
2022-01-03 02:36:23 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,23 +1,14 @@
 <?php
-include "koneksi.php";
-$id_user = $_POST['id_user'];
-$pass=md5($_POST['password']);
-$sql="SELECT * FROM users WHERE id_user='$id_user' AND password='$pass'";
-$login=mysqli_query($con,$sql);
-$ketemu=mysqli_num_rows($login);
-$r= mysqli_fetch_array($login);
-if ($ketemu > 0){
- session_start();
- $_SESSION['iduser'] = $r['id_user'];
- $_SESSION['passuser'] = $r['password'];
-echo"USER BERHASIL LOGIN<br>";
-echo "id user =",$_SESSION['iduser'],"<br>";
-echo "password=",$_SESSION['passuser'],"<br>";
-echo "<a href=logout.php><b>LOGOUT</b></a></center>";
-}
-else{
-echo "<center>Login gagal! username & password tidak benar<br>";
-echo "<a href=form_login.php><b>ULANGILAGI</b></a></center>";
-}
-mysqli_close($con);
+echo "<h2>Login</h2>
+<form method=post action=cek_login.php>
+<table>
+<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
+<tr><td>Password</td><td> : <input name='password' type='text'></td></tr>
+<tr><td>Captcha<br>
+
+<img src='captcha.php' /></td><td> : <input name='captcha_code' type='text'></td></tr>
+<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr>
+</table>
+</form>";
 ?>
+
